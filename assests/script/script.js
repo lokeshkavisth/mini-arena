f580f7eff4669ea2e363b8d3200a25c9def9cc1b
Lokesh Kavisth <104786100+lokeshkavisth@users.noreply.github.com>
2022-12-16 14:38:31 +0530

    Responsive

--- assests/script/script.js
+++ assests/script/script.js
@@ -7,8 +7,14 @@ const header = document.querySelector("[data-navbar]");
 window.addEventListener("scroll", function () {
   if (window.scrollY >= 1000) {
     header.classList.add("nav-active");
-    hideHeader();
   } else {
     header.classList.remove("nav-active");
   }
 });
+
+// mobile navbar
+function burgerClick() {
+  console.log("hello");
+
+  $(".nav-menu").slideToggle();
+}
